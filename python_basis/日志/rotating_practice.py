8ce089a57be5f432a3a6bc4fede641b360914ec7
15216 <15216802238@163.com>
2022-09-23 18:37:57 +0800

    人员滞留检测README

--- python_basis/日志/rotating_practice.py
+++ python_basis/日志/rotating_practice.py
@@ -7,8 +7,8 @@ logger = logging.getLogger("mingzhu")
 logger.setLevel(level=logging.INFO)#比level级别小的日志不会产生
 formatter = logging.Formatter('%(levelname)s %(asctime)s %(filename)s %(funcName)s line=%(lineno)d %(message)s',datefmt="%y-%m-%d %H:%M:%S")
 
-# 定义一个RotatingFileHandler，最多备份10个日志文件(超过10个就把最老的删掉，添加新的)，每个日志文件最大1M
-file_handler = RotatingFileHandler("log.txt", maxBytes=1*1024 , backupCount=10)
+# 定义一个RotatingFileHandler,最多备份10个日志文件(超过10个就把最老的删掉，添加新的)，每个日志文件最大maxBytes即最大字节数
+file_handler = RotatingFileHandler("./logs/log.txt", maxBytes=1*1024 , backupCount=10)
 file_handler.setLevel(logging.INFO)#比该level小的日志不会写到文件里
 file_handler.setFormatter(formatter)
 
@@ -20,11 +20,14 @@ logger.addHandler(file_handler)
 logger.addHandler(console)
 
 def output_info():
-    logger.info("first log information")
-    logger.debug("second log information")
-    logger.warning("third log information")
-    logger.info("fourth log information")
-    logger.info("fifth log information")
-    logger.info("sixth log information")
+    n = 1
+    while n < 25:
+        logger.info("first log information")
+        logger.debug("second log information")
+        logger.warning("third log information")
+        logger.info("fourth log information")
+        logger.info("fifth log information")
+        logger.info("sixth log information")
+        n += 1
 
-logger.info("logger本身设置level相当于限制产生与否;handler设置level相当于过滤")
+output_info()
