bca5cfecbbfe7925ab126697cebf140f161a7c6e
15216 <15216802238@163.com>
2022-10-11 10:44:15 +0800

    sklearn

--- topics/sklearn/sklearn保存与加载模型.py
+++ topics/sklearn/sklearn保存与加载模型.py
@@ -2,12 +2,28 @@ from sklearn import svm
 from sklearn import datasets
 import pickle
 import joblib
-clf = svm.SVC()
-iris = datasets.load_iris()
-X, y = iris.data, iris.target
-clf.fit(X, y)
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession, Row, SQLContext
+conf = SparkConf().setAppName("miniProject").setMaster("local[*]")
+# sc=SparkContext.getOrCreate(conf)
+spark = SparkSession.builder.config(conf=conf).enableHiveSupport().getOrCreate()
+sc = spark.sparkContext
 
-#使用spark读取
+def model_demo():
+    svm_model = svm.SVC()
+    iris = datasets.load_iris()
+    X, y = iris.data, iris.target
+    svm_model.fit(X, y)
+    return svm_model
+
+
+#sklearn模型对象通过spark保存的sc.parallelize([model]).saveAsPickleFile(full_path)，仍然通过spark读取
+# model = model_demo()
+# full_path = "file:///C:\\Users\\15216\\Desktop\\projects\\CodeCenter\\topics\\sklearn\\models\\svmmodel"
+
+save_by_spark = lambda model,full_path:sc.parallelize([model]).saveAsPickleFile(full_path)
+load_by_spark = lambda full_path:sc.pickleFile(full_path, 3).collect()[0]
+model = load_by_spark("file:///C:\\Users\\15216\\Desktop\\projects\\CodeCenter\\topics\\sklearn\\models\\pujulei\\model\\metadata")
 #方法一，使用dumps和loads,但没有存入磁盘
 # s = pickle.dumps(clf)
 # clf2 = pickle.loads(s)
